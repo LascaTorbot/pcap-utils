b26e1be796875c4890fe0c210954a71dc9b5e7dc
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2017-08-12 01:11:29 -0300

    First commit

---
+++ submit_exe.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import requests
+
+REST_URL = 'http://10.1.1.237:8090/tasks/create/file'
+PCAPS_PATH = '/home/quizumba/pcaps/exit-hourly'
+
+pcap_dir = os.path.join(PCAPS_PATH, 'pcap')
+
+# list *.gz files
+gzfiles = glob.glob("%s" % os.path.join(PCAPS_PATH, '*.gz'))
+
+for gzfile in gzfiles:
+    # extract files from .pcap.gz
+    os.system('mkdir %s' % pcap_dir)
+    os.system('cp %s %s' % (gzfile, pcap_dir))
+    os.system('cd %s && gunzip *.gz' % pcap_dir)
+
+    os.system('cd %s && tcpflow -r *.pcap -a' % pcap_dir)
+    os.system('cd %s && foremost -i *' % pcap_dir)
+
+    exe_files = glob.glob("%s" % os.path.join(pcap_dir, 'output', 'exe', '*'))
+    for exe_file in exe_files:
+        with open(exe_file, 'rb') as f:
+            files = {'file': ('temp_file_name', f)}
+            #r = requests.post(REST_URL, files=files)
+            print(files)
+
+    os.system('rm -rf %s' % pcap_dir)
